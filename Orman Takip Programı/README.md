7be30e00c1452d542a8050cf178699feb85d8381
samiloztoprak <33992781+samiloztoprak@users.noreply.github.com>
2021-05-01 17:59:08 +0300

    update

--- Orman Takip Programı/README.md
+++
@@ -1,2 +0,0 @@
-# Orman Takip Otomasyonu
-Access veri tabanı kullanılarak elazığ haritası üzerinden oyunlaştırma ile bölgesel ağaç takibinin sağlanmasını sağlayan otomasyon programı. 
---
+++ parmak_izi/delete.ino
@@ -0,0 +1,33 @@
+
+uint8_t readnumber(void) {
+  uint8_t num = 0;
+  
+  while (num == 0) {
+    while (! Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
+}
+
+
+uint8_t deleteFingerprint(uint8_t id) {
+  uint8_t p = -1;
+  
+  p = finger.deleteModel(id);
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Deleted!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not delete in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.print("Unknown error: 0x"); Serial.println(p, HEX);
+    return p;
+  }   
+}
